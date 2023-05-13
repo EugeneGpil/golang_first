20dca19bdd4fe94931e3ad467a808a90ddb09603
EugeneGpil <gpilligrim@gmail.com>
2023-05-13 23:01:30 +0300

    1

--- back/003_flow_control_statements/002_for_continue/for_continue.go
+++ back/003_flow_control_statements/002_for_continue/for_continue.go
@@ -4,9 +4,16 @@ import "fmt"
 
 func main() {
 	sum := 1
+	// init and post statements are optional
 	for ; sum < 1000; {
 		sum += sum
 	}
+	fmt.Println(sum)
 
+	sum = 1
+	// and at this point can drop semicolons
+	for sum < 1000 {
+		sum += sum
+	}
 	fmt.Println(sum)
 }
