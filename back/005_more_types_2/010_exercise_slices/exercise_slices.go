f83cc3a5ddc293387527d375fab1959d52716408
EugeneGpil <gpilligrim@gmail.com>
2023-05-18 20:21:03 +0300

    1

--- back/005_more_types_2/010_exercise_slices/exercise_slices.go
+++ back/005_more_types_2/010_exercise_slices/exercise_slices.go
@@ -11,11 +11,11 @@ func main() {
 func Pic(dx, dy int) [][]uint8 {
 	matrix := make([][]uint8, dy)
 
-	for row_key := range matrix {
-		matrix[row_key] = make([]uint8, dx)
+	for rowKey := range matrix {
+		matrix[rowKey] = make([]uint8, dx)
 		
-		for column_key := range matrix[row_key] {
-			matrix[row_key][column_key] = uint8(column_key * row_key)
+		for columnKey := range matrix[rowKey] {
+			matrix[rowKey][columnKey] = uint8(columnKey * rowKey)
 			// matrix[row_key][column_key] = uint8((column_key + row_key) / 2)
 			// matrix[row_key][column_key] = uint8(column_key * 2 + row_key / 2)
 			// matrix[row_key][column_key] = uint8(math.Pow(float64(row_key), float64(column_key)))
