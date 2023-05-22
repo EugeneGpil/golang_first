462c3063f009cd4e315f831fd1a894efcc3cd170
EugeneGpil <gpilligrim@gmail.com>
2023-05-22 20:57:57 +0300

    1

---
+++ back/009_interfaces/005_nil_interface_values/go.mod
@@ -0,0 +1,3 @@
+module github.com/nil_interface_values
+
+go 1.20
---
+++ back/009_interfaces/005_nil_interface_values/nil_interface_values.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+type MyInterface interface {
+	MyMethod()
+}
+
+func main() {
+	var myInterfaceVar MyInterface
+	describe(myInterfaceVar)
+	myInterfaceVar.MyMethod()
+}
+
+func describe(myInterfaceVar MyInterface) {
+	fmt.Printf("(%v, %T)\n", myInterfaceVar, myInterfaceVar)
+}
