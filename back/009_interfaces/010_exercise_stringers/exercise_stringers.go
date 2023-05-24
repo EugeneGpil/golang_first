b3184ad1bab0f044fa62dcdc3ad52189e01887ad
EugeneGpil <gpilligrim@gmail.com>
2023-05-24 20:54:14 +0300

    1

---
+++ back/009_interfaces/010_exercise_stringers/exercise_stringers.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+type IpAddr [4]byte
+
+func (ipAddr IpAddr) String() string {
+	return fmt.Sprintf("%v.%v.%v.%v", ipAddr[0], ipAddr[1], ipAddr[2], ipAddr[3])
+}
+
+func main() {
+	hosts := map[string]IpAddr{
+		"loopback":  {127, 0, 0, 1},
+		"googleDNS": {8, 8, 8, 8},
+	}
+	for name, ip := range hosts {
+		fmt.Printf("%v: %v\n", name, ip)
+	}
+}
---
+++ back/009_interfaces/010_exercise_stringers/go.mod
@@ -0,0 +1,3 @@
+module github.com/exercise_stringers
+
+go 1.20
